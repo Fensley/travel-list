a7802f4c61a3c1a2f39c4852d6a023ac0b4c2918
Fensley <zagalufe@gmail.com>
2024-04-19 00:29:34 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 3, description: "Charger", quantity: 1, packed: false },
+];
 export default function App() {
-  const initialItems = [
-    { id: 1, description: "Passports", quantity: 2, packed: false },
-    { id: 2, description: "Socks", quantity: 12, packed: false },
-  ];
-
   return (
     <div className="app">
       <Logo />
@@ -19,16 +19,30 @@ function Logo() {
 }
 function Form() {
   return (
-    <di className="add-form">
+    <div className="add-form">
       <h3> What do you need for your 😍 trip</h3>
-    </di>
+    </div>
   );
 }
 function ParckingList() {
   return (
     <div className="list">
-      <ul>LIST</ul>
+      <ul>
+        {initialItems.map((each) => (
+          <Item Item={each} key={each.id} />
+        ))}
+      </ul>
     </div>
+  );
+}
+function Item({ Item }) {
+  return (
+    <li>
+      <span style={Item.packed ? { textDecoration: "line-through" } : {}}>
+        {Item.quantity} {Item.description}
+      </span>
+      <button>❌</button>
+    </li>
   );
 }
 function Stats() {
